b12fdb81549cdebeb2dd925f4fe6b6697759c955
aspectrr <collin@steelbrowser.com>
2025-09-07 19:46:17 -0400

    chore: update packages

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,14 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
-export default {
+export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    connectionString:
+    url:
       process.env.DATABASE_URL ||
       `postgresql://${process.env.DB_USER || "postgres"}:${process.env.DB_PASSWORD || "password"}@${process.env.DB_HOST || "localhost"}:${process.env.DB_PORT || "5432"}/${process.env.DB_NAME || "acacia_store"}`,
   },
   verbose: true,
   strict: true,
-  migrations: {
-    prefix: "timestamp",
-  },
-} satisfies Config;
+});
